7d81610e00c9139d45458d9fdb2004e48cf24ed1
Erwin Mueller <erwin.mueller@deventm.org>
2012-05-07 17:42:08 +0800

    Finish test if the user closed the window

--- resources/src/test/groovy/com/anrisoftware/resources/ShowIconFrame.groovy
+++ resources/src/test/groovy/com/anrisoftware/resources/ShowIconFrame.groovy
@@ -1,5 +1,7 @@
 package com.anrisoftware.resources;
 
+import static javax.swing.SwingUtilities.invokeLater
+
 import java.awt.BorderLayout
 import java.awt.GridLayout
 import java.awt.event.ActionListener
@@ -12,35 +14,42 @@ import javax.swing.JPanel
 
 class ShowIconFrame {
 
-	def images = []
+	def images
 
-	def duration = 60000
+	long timeout = 60000
 
-	def call() {
-		def frame = new JFrame("Icon Show")
-		frame.layout = new BorderLayout()
+	def synchronized call() {
+		invokeLater {
+			def frame = new JFrame("Icon Show")
+			frame.layout = new BorderLayout()
 
-		def count = Math.ceil(getImages().size() / 2) as int
-		def panel = new JPanel(new GridLayout(count, count, 4, 4))
-		getImages().each {
-			def label = new JLabel(new ImageIcon(it))
-			panel.add label
+			def count = Math.ceil(getImages().size() / 2) as int
+			def panel = new JPanel(new GridLayout(count, count, 4, 4))
+			getImages().each {
+				def label = new JLabel(new ImageIcon(it))
+				panel.add label
+			}
+
+			def button = new JButton("Close")
+			button.addActionListener({e ->
+				frame.visible = false
+				synchronized (owner.owner) {
+					owner.owner.notifyAll()
+				}
+			}as ActionListener)
+
+			frame.add panel, BorderLayout.CENTER
+			frame.add button, BorderLayout.SOUTH
+
+			frame.setSize 300, 300
+			frame.defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
+			frame.rootPane.defaultButton = button
+			frame.visible = true
 		}
-
-		def button = new JButton("Close")
-		button.addActionListener({e -> frame.visible = false }as ActionListener)
-
-		frame.add panel, BorderLayout.CENTER
-		frame.add button, BorderLayout.SOUTH
-
-		frame.setSize 300, 300
-		frame.defaultCloseOperation = JFrame.DISPOSE_ON_CLOSE
-		frame.rootPane.defaultButton = button
-		frame.visible = true
-		Thread.sleep duration
+		wait timeout
 	}
 
-	def getImages() {
+	List getImages() {
 		if (images instanceof List) {
 			return images
 		} else {
