7fd32562772b090b260961f4585b7d3d32bcef1b
Erwin Mueller <erwin.mueller@deventm.org>
2012-06-08 19:06:22 +0800

    Add serialization test from tests module

--- resources-texts/src/test/groovy/com/anrisoftware/resources/texts/SerializeTextResourceTest.groovy
+++ resources-texts/src/test/groovy/com/anrisoftware/resources/texts/SerializeTextResourceTest.groovy
@@ -2,25 +2,22 @@ package com.anrisoftware.resources.texts
 
 import org.junit.Test
 
-import com.anrisoftware.resources.api.TextResourceFactory
-import com.google.common.base.Charsets
+import com.anrisoftware.resources.texts.maps.ResourcesTextsMapsModule
 
-class SerializeTextResourceTest extends TextResourceTest {
+class SerializeTextResourceTest extends AbstractSerializationTest {
 
 	@Test
 	void "serialize text resource"() {
-		TextResourceFactory factory = injector.getInstance TextResourceFactory
-		def name = "Text Resource A"
-		def locale = Locale.ENGLISH
-		def url = new URL("file://")
-		def charset = Charsets.UTF_8
+		super."serialize text resource"()
+	}
 
-		def resource = factory.create name, locale, url, charset
-		def resourceB = reserialize resource
+	@Override
+	Object getTextsModule() {
+		new ResourcesTextsModule()
+	}
 
-		assert resourceB.name == name
-		assert resourceB.locale == locale
-		assert resourceB.url == url
-		assert resourceB.charset == charset
+	@Override
+	Object getTextsMapModule() {
+		new ResourcesTextsMapsModule()
 	}
 }
