0fc81021535d0a2729edd77365cb66515d496384
Erwin Mueller <erwin.mueller@deventm.org>
2012-06-11 11:25:42 +0800

    Sets the javadoc for the resources exception
    #1264

--- resources-api/src/main/java/com/anrisoftware/resources/api/ResourcesException.java
+++ resources-api/src/main/java/com/anrisoftware/resources/api/ResourcesException.java
@@ -4,15 +4,51 @@ import static java.lang.String.format;
 
 import java.util.MissingResourceException;
 
+/**
+ * Exception that is thrown if there was an error loading a resource.
+ * 
+ * @author Erwin Mueller, erwin.mueller@deventm.org
+ * @since 1.0
+ */
 @SuppressWarnings("serial")
 public class ResourcesException extends MissingResourceException {
 
+	/**
+	 * Sets the cause for this exception with a message.
+	 * 
+	 * @param cause
+	 *            the {@link Throwable} cause.
+	 * 
+	 * @param key
+	 *            the {@link String} key for the missing resource.
+	 * 
+	 * @param format
+	 *            a format {@link String} for the exception message.
+	 * 
+	 * @param args
+	 *            the optional {@link Object}s for the format message.
+	 */
 	public ResourcesException(Throwable cause, String key, String format,
 			Object... args) {
 		super(format("%s: %s", cause.getMessage(), format(format, args)), "",
 				key);
 	}
 
+	/**
+	 * Sets the class name, key and a formatted messsage.
+	 * 
+	 * @param className
+	 *            the {@link String} name of the resource class
+	 * 
+	 * @param key
+	 *            the {@link String} key for the missing resource.
+	 * 
+	 * @param format
+	 *            a format {@link String} for the exception message.
+	 * 
+	 * @param args
+	 *            the optional {@link Object}s for the format message.
+	 */
 	public ResourcesException(String className, String key, String format,
 			Object... args) {
 		super(format(format, args), className, key);
