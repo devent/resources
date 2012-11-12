b1c6cb9e194015259acefdc63d4321490364f74b
Erwin Mueller <erwin.mueller@deventm.org>
2012-11-12 03:54:55 +0100

    Add ctors to set the default character set for text resources

--- resources-texts/src/main/java/com/anrisoftware/resources/texts/ResourcesTextsCharsetModule.java
+++ resources-texts/src/main/java/com/anrisoftware/resources/texts/ResourcesTextsCharsetModule.java
@@ -1,18 +1,18 @@
 /*
  * Copyright 2012 Erwin Müller <erwin.mueller@deventm.org>
- * 
+ *
  * This file is part of resources-texts.
- * 
+ *
  * resources-texts is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * resources-texts is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with resources-texts. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,16 +25,40 @@ import java.nio.charset.Charset;
 import com.google.inject.AbstractModule;
 
 /**
- * Binds the texts recourses character set to the default system characater set.
+ * Binds the texts recourses default character set. The default character set is
+ * used if no character set is specified in the text resource.
  * 
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 1.0
  */
 public class ResourcesTextsCharsetModule extends AbstractModule {
 
+	private final Charset defaultCharset;
+
+	/**
+	 * Sets the default character set of the JVM as the default character set
+	 * for text resources.
+	 * 
+	 * @see Charset#defaultCharset()
+	 * 
+	 * @since 1.3
+	 */
+	public ResourcesTextsCharsetModule() {
+		this(Charset.defaultCharset());
+	}
+
+	/**
+	 * Sets the specified character set as the default character set for text
+	 * resources.
+	 * 
+	 * @param charset
+	 */
+	public ResourcesTextsCharsetModule(Charset charset) {
+		this.defaultCharset = charset;
+	}
+
 	@Override
 	protected void configure() {
-		Charset defaultCharset = Charset.defaultCharset();
 		bind(Charset.class).annotatedWith(named("texts-default-charset"))
 				.toInstance(defaultCharset);
 	}
