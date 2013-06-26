9bdd305ff6e16ae1e91e484d0c426d6201528a80
Erwin Mueller <erwin.mueller@deventm.org>
2013-06-26 23:10:47 +0200

    Rework template resources logging
    #2474

--- resources-templates/src/main/java/com/anrisoftware/resources/templates/maps/TemplatesMapLogger.java
+++ resources-templates/src/main/java/com/anrisoftware/resources/templates/maps/TemplatesMapLogger.java
@@ -1,20 +1,14 @@
 /*
- * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org>
- *
- * This file is part of resources-templates.
- *
- * resources-templates is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- *
- * resources-templates is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with resources-templates. If not, see <http://www.gnu.org/licenses/>.
+ * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org> This file is
+ * part of resources-templates. resources-templates is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version. resources-templates is
+ * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details. You
+ * should have received a copy of the GNU Lesser General Public License along
+ * with resources-templates. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.resources.templates.maps;
 
@@ -28,6 +22,8 @@ import com.anrisoftware.globalpom.log.AbstractLogger;
  */
 class TemplatesMapLogger extends AbstractLogger {
 
+	private static final String CONTAINS_RESOURCE = "Map contains template resource '{}'.";
+
 	/**
 	 * Creates a logger for {@link TemplatesMapImpl}.
 	 */
@@ -36,7 +32,7 @@ class TemplatesMapLogger extends AbstractLogger {
 	}
 
 	void textAlreadyInMap(String name) {
-		log.warn("We contain already the template resource ``{}''.", name);
+		log.warn(CONTAINS_RESOURCE, name);
 	}
 
 }
