658a0e01a64dcf7c6bc4f6033ec4ea10b7e9a029
Erwin Mueller <erwin.mueller@deventm.org>
2013-06-26 23:01:19 +0200

    Rework text resources logging
    #2474

--- resources-texts/src/main/java/com/anrisoftware/resources/texts/maps/TextsMapLogger.java
+++ resources-texts/src/main/java/com/anrisoftware/resources/texts/maps/TextsMapLogger.java
@@ -1,20 +1,14 @@
 /*
- * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org>
- *
- * This file is part of resources-texts.
- *
- * resources-texts is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- *
- * resources-texts is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with resources-texts. If not, see <http://www.gnu.org/licenses/>.
+ * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org> This file is
+ * part of resources-texts. resources-texts is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version. resources-texts is
+ * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details. You
+ * should have received a copy of the GNU Lesser General Public License along
+ * with resources-texts. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.resources.texts.maps;
 
@@ -28,6 +22,8 @@ import com.anrisoftware.globalpom.log.AbstractLogger;
  */
 class TextsMapLogger extends AbstractLogger {
 
+	private static final String CONTAINS_TEXT = "Map contains text resource '{}'.";
+
 	/**
 	 * Creates a logger for {@link TextsMapImpl}.
 	 */
@@ -36,7 +32,7 @@ class TextsMapLogger extends AbstractLogger {
 	}
 
 	void textAlreadyInMap(String name) {
-		log.warn("We contain already the text ``{}''.", name);
+		log.warn(CONTAINS_TEXT, name);
 	}
 
 }
--- resources-texts/src/main/java/com/anrisoftware/resources/texts/texts/TextResourceImplLogger.java
+++ resources-texts/src/main/java/com/anrisoftware/resources/texts/texts/TextResourceImplLogger.java
@@ -1,26 +1,19 @@
 /*
- * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org>
- *
- * This file is part of resources-texts.
- *
- * resources-texts is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
- *
- * resources-texts is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with resources-texts. If not, see <http://www.gnu.org/licenses/>.
+ * Copyright 2012-2013 Erwin Müller <erwin.mueller@deventm.org> This file is
+ * part of resources-texts. resources-texts is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version. resources-texts is
+ * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details. You
+ * should have received a copy of the GNU Lesser General Public License along
+ * with resources-texts. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.anrisoftware.resources.texts.texts;
 
-import static java.lang.String.format;
-
 import java.io.IOException;
+import java.util.Locale;
 
 import com.anrisoftware.globalpom.log.AbstractLogger;
 import com.anrisoftware.resources.api.ResourcesException;
@@ -33,7 +26,10 @@ import com.anrisoftware.resources.api.ResourcesException;
  */
 class TextResourceImplLogger extends AbstractLogger {
 
-	private static final String ERROR_LOAD_TEXT = "Error loading text resource '%s' (%s)";
+	private static final String LOCALE = "locale";
+	private static final String NAME = "name";
+	private static final String ERROR_LOAD_MESSAGE = "Error load text resource '{}' ({})";
+	private static final String ERROR_LOAD = "Error load text resource";
 
 	/**
 	 * Creates a logger for {@link TextResourceImpl}.
@@ -46,11 +42,9 @@ class TextResourceImplLogger extends AbstractLogger {
 
 	ResourcesException errorLoadText(TextResourceImpl res, IOException e) {
 		String name = res.getName();
-		String message = format(ERROR_LOAD_TEXT, name, res.getLocale());
-		ResourcesException ex = new ResourcesException(e, message, null, name);
-		ex.addContext("locale", res.getLocale());
-		logException(ex, message);
-		return ex;
+		Locale locale = res.getLocale();
+		return logException(new ResourcesException(e, ERROR_LOAD, null, name)
+				.addContext(NAME, name).addContext(LOCALE, locale),
+				ERROR_LOAD_MESSAGE, name, locale);
 	}
-
 }
