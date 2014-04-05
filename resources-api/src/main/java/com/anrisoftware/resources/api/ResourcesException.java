a94497dbffa4bccc9514debc192882a379b723a6
Erwin Mueller <erwin.mueller@deventm.org>
2014-04-05 18:32:25 +0200

    Update dependencies.

--- resources-api/src/main/java/com/anrisoftware/resources/api/ResourcesException.java
+++ resources-api/src/main/java/com/anrisoftware/resources/api/ResourcesException.java
@@ -27,98 +27,98 @@ import com.anrisoftware.globalpom.exceptions.Context;
 
 /**
  * Exception that is thrown if there was an error loading a resource.
- * 
+ *
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 1.0
  */
 @SuppressWarnings("serial")
 public class ResourcesException extends MissingResourceException {
 
-	private final Context<ResourcesException> context;
+    private final Context<ResourcesException> context;
 
-	private final Throwable cause;
+    private final Throwable cause;
 
-	/**
-	 * @see MissingResourceException#MissingResourceException(String, String,
-	 *      String)
-	 * 
-	 * @since 1.7
-	 */
-	public ResourcesException(String message, String className, String key) {
-		this(null, message, className, key);
-	}
+    /**
+     * @see MissingResourceException#MissingResourceException(String, String,
+     *      String)
+     *
+     * @since 1.7
+     */
+    public ResourcesException(String message, String className, String key) {
+        this(null, message, className, key);
+    }
 
-	/**
-	 * @param cause
-	 *            the {@link Throwable} cause of the exception.
-	 * 
-	 * @see MissingResourceException#MissingResourceException(String, String,
-	 *      String)
-	 * 
-	 * @since 1.7
-	 */
-	public ResourcesException(Throwable cause, String message,
-			String className, String key) {
-		super(message, className, key);
-		this.context = new Context<ResourcesException>(this);
-		this.cause = cause;
-		if (cause != null) {
-			addContext("cause", cause);
-		}
-		if (!isEmpty(key)) {
-			addContext("key", key);
-		}
-		if (!isEmpty(className)) {
-			addContext("class name", className);
-		}
-	}
+    /**
+     * @param cause
+     *            the {@link Throwable} cause of the exception.
+     *
+     * @see MissingResourceException#MissingResourceException(String, String,
+     *      String)
+     *
+     * @since 1.7
+     */
+    public ResourcesException(Throwable cause, String message,
+            String className, String key) {
+        super(message, className, key);
+        this.context = new Context<ResourcesException>(this);
+        this.cause = cause;
+        if (cause != null) {
+            addContext("cause", cause);
+        }
+        if (!isEmpty(key)) {
+            addContext("key", key);
+        }
+        if (!isEmpty(className)) {
+            addContext("class name", className);
+        }
+    }
 
-	/**
-	 * Adds the context with the specified name.
-	 * 
-	 * @param name
-	 *            the name of the context.
-	 * 
-	 * @param value
-	 *            the context value.
-	 * 
-	 * @return the context {@link Exception}.
-	 * 
-	 * @since 1.7
-	 */
-	public ResourcesException addContext(String name, Object value) {
-		return context.addContext(name, value);
-	}
+    /**
+     * Adds the context with the specified name.
+     *
+     * @param name
+     *            the name of the context.
+     *
+     * @param value
+     *            the context value.
+     *
+     * @return the context {@link Exception}.
+     *
+     * @since 1.7
+     */
+    public ResourcesException addContext(String name, Object value) {
+        return context.addContext(name, value);
+    }
 
-	/**
-	 * Returns the context of the exception.
-	 * 
-	 * @return an unmodifiable {@link Map} with the context.
-	 * 
-	 * @since 1.7
-	 */
-	public Map<String, Object> getContext() {
-		return context.getContext();
-	}
+    /**
+     * Returns the context of the exception.
+     *
+     * @return an unmodifiable {@link Map} with the context.
+     *
+     * @since 1.7
+     */
+    public Map<String, Object> getContext() {
+        return context.getContext();
+    }
 
-	@Override
-	public synchronized Throwable getCause() {
-		return (cause == this ? null : cause);
-	}
+    @Override
+    public synchronized Throwable getCause() {
+        return (cause == this ? null : cause);
+    }
 
-	/**
-	 * Output the exception message and the context:
-	 * 
-	 * <pre>
-	 * Message of the exception, context:
-	 * Aaa := bbb
-	 * Ccc := ddd
-	 * </pre>
-	 * 
-	 * @since 1.7
-	 */
-	@Override
-	public String toString() {
-		return context.toString();
-	}
+    /**
+     * Output the exception message and the context:
+     *
+     * <pre>
+     * Message of the exception, context:
+     * Aaa := bbb
+     * Ccc := ddd
+     * </pre>
+     *
+     * @since 1.7
+     */
+    @Override
+    public String toString() {
+        return context.toString();
+    }
 }
