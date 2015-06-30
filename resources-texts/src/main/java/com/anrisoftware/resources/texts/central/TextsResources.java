9b3bc5a49d04b2882824be22b368be7e073b5537
Erwin Mueller <erwin.mueller@deventm.org>
2015-06-30 14:18:51 +0200

    Documentation.

--- resources-texts/src/main/java/com/anrisoftware/resources/texts/central/TextsResources.java
+++ resources-texts/src/main/java/com/anrisoftware/resources/texts/central/TextsResources.java
@@ -39,236 +39,350 @@ import com.google.inject.assistedinject.AssistedInject;
 
 /**
  * Centralized access to all text resources.
- * 
+ *
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 1.4
  */
 public class TextsResources {
 
-	/**
-	 * Action accelerator keys resource bundle.
-	 */
-	public static final String ACTION_ACCELERATORS_PROPERTY = "action_accelerators";
+    /**
+     * Action accelerator keys resource bundle.
+     */
+    public static final String ACTION_ACCELERATORS_PROPERTY = "action_accelerators";
 
-	/**
-	 * Action mnemonics resource bundle.
-	 */
-	public static final String ACTION_MNEMONICS_PROPERTY = "action_mnemonics";
+    /**
+     * Action mnemonics resource bundle.
+     */
+    public static final String ACTION_MNEMONICS_PROPERTY = "action_mnemonics";
 
-	/**
-	 * Action names resource bundle.
-	 */
-	public static final String ACTIONS_PROPERTY = "actions";
+    /**
+     * Action names resource bundle.
+     */
+    public static final String ACTIONS_PROPERTY = "actions";
 
-	/**
-	 * Texts resource bundle.
-	 */
-	public static final String TEXTS_PROPERTY = "texts";
+    /**
+     * Texts resource bundle.
+     */
+    public static final String TEXTS_PROPERTY = "texts";
 
-	private final EventListenerSupport<LocaleListener> localeListeners;
+    private final EventListenerSupport<LocaleListener> localeListeners;
 
-	private final MnemonicFactory mnemonicFactory;
+    private final MnemonicFactory mnemonicFactory;
 
-	private final AcceleratorFactory acceleratorFactory;
+    private final AcceleratorFactory acceleratorFactory;
 
-	private Texts texts;
+    private Texts texts;
 
-	private Texts actions;
+    private Texts actions;
 
-	private Texts mnemonics;
+    private Texts mnemonics;
 
-	private Texts accelerators;
+    private Texts accelerators;
 
-	private Locale locale;
+    private Locale locale;
 
-	/**
-	 * Sets default properties.
-	 * 
-	 * @since 1.6
-	 */
-	@AssistedInject
-	TextsResources(TextsFactory textsFactory, MnemonicFactory mnemonicFactory,
-			AcceleratorFactory acceleratorFactory) {
-		this(textsFactory, mnemonicFactory, acceleratorFactory,
-				createDefaultProperties());
-	}
+    /**
+     * Sets default properties.
+     *
+     * @since 1.6
+     */
+    @AssistedInject
+    TextsResources(TextsFactory textsFactory, MnemonicFactory mnemonicFactory,
+            AcceleratorFactory acceleratorFactory) {
+        this(textsFactory, mnemonicFactory, acceleratorFactory,
+                createDefaultProperties());
+    }
 
-	private static Properties createDefaultProperties() {
-		Properties p = new Properties();
-		p.setProperty(TEXTS_PROPERTY, "Texts");
-		p.setProperty(ACTIONS_PROPERTY, "Actions");
-		p.setProperty(ACTION_MNEMONICS_PROPERTY, "ActionMnemonics");
-		p.setProperty(ACTION_ACCELERATORS_PROPERTY, "ActionAccelerators");
-		return p;
-	}
+    private static Properties createDefaultProperties() {
+        Properties p = new Properties();
+        p.setProperty(TEXTS_PROPERTY, "Texts");
+        p.setProperty(ACTIONS_PROPERTY, "Actions");
+        p.setProperty(ACTION_MNEMONICS_PROPERTY, "ActionMnemonics");
+        p.setProperty(ACTION_ACCELERATORS_PROPERTY, "ActionAccelerators");
+        return p;
+    }
 
-	/**
-	 * @see TextsResourcesFactory#create(Properties)
-	 * 
-	 * @since 1.6
-	 */
-	@AssistedInject
-	TextsResources(TextsFactory textsFactory, MnemonicFactory mnemonicFactory,
-			AcceleratorFactory acceleratorFactory,
-			@Assisted Properties properties) {
-		if (properties.containsKey(TEXTS_PROPERTY)) {
-			this.texts = textsFactory.create(properties
-					.getProperty(TEXTS_PROPERTY));
-		}
-		if (properties.containsKey(ACTIONS_PROPERTY)) {
-			this.actions = textsFactory.create(properties
-					.getProperty(ACTIONS_PROPERTY));
-		}
-		if (properties.containsKey(ACTION_MNEMONICS_PROPERTY)) {
-			this.mnemonics = textsFactory.create(properties
-					.getProperty(ACTION_MNEMONICS_PROPERTY));
-		}
-		if (properties.containsKey(ACTION_ACCELERATORS_PROPERTY)) {
-			this.accelerators = textsFactory.create(properties
-					.getProperty(ACTION_ACCELERATORS_PROPERTY));
-		}
-		this.localeListeners = new EventListenerSupport<LocaleListener>(
-				LocaleListener.class);
-		this.locale = Locale.getDefault();
-		this.mnemonicFactory = mnemonicFactory;
-		this.acceleratorFactory = acceleratorFactory;
-	}
+    /**
+     * @see TextsResourcesFactory#create(Properties)
+     *
+     * @since 1.6
+     */
+    @AssistedInject
+    TextsResources(TextsFactory textsFactory, MnemonicFactory mnemonicFactory,
+            AcceleratorFactory acceleratorFactory,
+            @Assisted Properties properties) {
+        if (properties.containsKey(TEXTS_PROPERTY)) {
+            this.texts = textsFactory.create(properties
+                    .getProperty(TEXTS_PROPERTY));
+        }
+        if (properties.containsKey(ACTIONS_PROPERTY)) {
+            this.actions = textsFactory.create(properties
+                    .getProperty(ACTIONS_PROPERTY));
+        }
+        if (properties.containsKey(ACTION_MNEMONICS_PROPERTY)) {
+            this.mnemonics = textsFactory.create(properties
+                    .getProperty(ACTION_MNEMONICS_PROPERTY));
+        }
+        if (properties.containsKey(ACTION_ACCELERATORS_PROPERTY)) {
+            this.accelerators = textsFactory.create(properties
+                    .getProperty(ACTION_ACCELERATORS_PROPERTY));
+        }
+        this.localeListeners = new EventListenerSupport<LocaleListener>(
+                LocaleListener.class);
+        this.locale = Locale.getDefault();
+        this.mnemonicFactory = mnemonicFactory;
+        this.acceleratorFactory = acceleratorFactory;
+    }
 
-	public void setTexts(Texts texts) {
-		Texts oldValue = this.texts;
-		this.texts = texts;
-		if (oldValue != texts) {
-			fireUpdateLocale();
-		}
-	}
+    /**
+     * Sets the texts resources.
+     *
+     * @param texts
+     *            the {@link Texts}.
+     */
+    public void setTexts(Texts texts) {
+        Texts oldValue = this.texts;
+        this.texts = texts;
+        if (oldValue != texts) {
+            fireUpdateLocale();
+        }
+    }
 
-	public Texts getTexts() {
-		return texts;
-	}
+    /**
+     * Returns the texts resources.
+     *
+     * @return the {@link Texts}.
+     */
+    public Texts getTexts() {
+        return texts;
+    }
 
-	public void setActions(Texts actions) {
-		Texts oldValue = this.actions;
-		this.actions = actions;
-		if (oldValue != actions) {
-			fireUpdateLocale();
-		}
-	}
+    /**
+     * Sets the texts resources for the actions names.
+     *
+     * @param actions
+     *            the {@link Texts}.
+     */
+    public void setActions(Texts actions) {
+        Texts oldValue = this.actions;
+        this.actions = actions;
+        if (oldValue != actions) {
+            fireUpdateLocale();
+        }
+    }
 
-	public Texts getActions() {
-		return actions;
-	}
+    /**
+     * Returns the texts resources for the actions names.
+     *
+     * @return the {@link Texts}.
+     */
+    public Texts getActions() {
+        return actions;
+    }
 
-	public void setMnemonics(Texts mnemonics) {
-		Texts oldValue = this.mnemonics;
-		this.mnemonics = mnemonics;
-		if (oldValue != mnemonics) {
-			fireUpdateLocale();
-		}
-	}
+    /**
+     * Sets the texts resources for the mnemonics.
+     *
+     * @param mnemonics
+     *            the {@link Texts}.
+     */
+    public void setMnemonics(Texts mnemonics) {
+        Texts oldValue = this.mnemonics;
+        this.mnemonics = mnemonics;
+        if (oldValue != mnemonics) {
+            fireUpdateLocale();
+        }
+    }
 
-	public Texts getMnemonics() {
-		return mnemonics;
-	}
+    /**
+     * Returns the texts resources for the mnemonics.
+     *
+     * @return the {@link Texts}.
+     */
+    public Texts getMnemonics() {
+        return mnemonics;
+    }
 
-	public void setAccelerators(Texts accelerators) {
-		Texts oldValue = this.accelerators;
-		this.accelerators = accelerators;
-		if (oldValue != accelerators) {
-			fireUpdateLocale();
-		}
-	}
+    /**
+     * Sets the texts resources for the accelerator keys.
+     *
+     * @param accelerators
+     *            the {@link Texts}.
+     */
+    public void setAccelerators(Texts accelerators) {
+        Texts oldValue = this.accelerators;
+        this.accelerators = accelerators;
+        if (oldValue != accelerators) {
+            fireUpdateLocale();
+        }
+    }
 
-	public Texts getAccelerators() {
-		return accelerators;
-	}
+    /**
+     * Returns the texts resources for the accelerator keys.
+     *
+     * @return the {@link Texts}.
+     */
+    public Texts getAccelerators() {
+        return accelerators;
+    }
 
-	public void setLocale(Locale locale) {
-		this.locale = locale;
-		fireUpdateLocale();
-	}
+    /**
+     * Sets the locale for the texts resources.
+     *
+     * @param locale
+     *            the {@link Locale}.
+     */
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+        fireUpdateLocale();
+    }
 
-	public Locale getLocale() {
-		return locale;
-	}
+    /**
+     * Returns the locale for the texts resources.
+     *
+     * @return the {@link Locale}.
+     */
+    public Locale getLocale() {
+        return locale;
+    }
 
-	private void fireUpdateLocale() {
-		localeListeners.fire().updateLocale(locale);
-	}
+    private void fireUpdateLocale() {
+        localeListeners.fire().updateLocale(locale);
+    }
 
-	public String getText(String name) throws ResourcesException {
-		return texts.getResource(name, locale).getText();
-	}
+    /**
+     * Returns the text from the texts resources.
+     *
+     * @return the {@link String} text.
+     *
+     * @throws ResourcesException
+     *             if there was an error loading the text.
+     */
+    public String getText(String name) throws ResourcesException {
+        return texts.getResource(name, locale).getText();
+    }
 
-	public String getTextFormat(String name, Object... args)
-			throws ResourcesException {
-		return texts.getResource(name, locale).getFormattedText(args);
-	}
+    /**
+     * Returns a formatted text from the texts resource.
+     * <p>
+     * The string is formatted as in
+     * {@link String#format(Locale, String, Object...)} with the current text as
+     * the format string and the current locale as the locale.
+     *
+     * @param args
+     *            the arguments.
+     *
+     * @return the {@link String} text.
+     *
+     * @throws ResourcesException
+     *             if there was an error loading the text.
+     */
+    public String getTextFormat(String name, Object... args)
+            throws ResourcesException {
+        return texts.getResource(name, locale).getFormattedText(args);
+    }
 
-	public String getAction(String name) throws ResourcesException {
-		return actions.getResource(name, locale).getText();
-	}
+    /**
+     * Returns the action name from the actions texts resources.
+     *
+     * @param name
+     *            the {@link String} resource name.
+     *
+     * @return the {@link String} action name.
+     *
+     * @throws ResourcesException
+     *             if there was an error loading the text.
+     */
+    public String getAction(String name) throws ResourcesException {
+        return actions.getResource(name, locale).getText();
+    }
 
-	/**
-	 * Loads a mnemonic text resource with the specified name and returns the
-	 * mnemonic key code.
-	 * 
-	 * @see Mnemonic#getMnemonic()
-	 * 
-	 * @param name
-	 *            the mnemonic text resource name.
-	 * 
-	 * @throws ResourcesException
-	 *             if there was an error loading the text resource.
-	 */
-	public int getMnemonic(String name) throws ResourcesException {
-		String keyname = mnemonics.getResource(name, locale).getText();
-		return mnemonicFactory.create(keyname).getMnemonic();
-	}
+    /**
+     * Returns the mnemonic key code from the mnemonics texts resources.
+     *
+     * @see Mnemonic#getMnemonic()
+     *
+     * @param name
+     *            the {@link String} resource name.
+     *
+     * @return the {@link Integer} mnemonic key code.
+     *
+     * @throws ResourcesException
+     *             if there was an error loading the text resource.
+     */
+    public int getMnemonic(String name) throws ResourcesException {
+        String keyname = mnemonics.getResource(name, locale).getText();
+        return mnemonicFactory.create(keyname).getMnemonic();
+    }
 
-	/**
-	 * Loads a mnemonic text resource with the specified name and returns the
-	 * displayed mnemonic index if such index was set in the resource.
-	 * 
-	 * @see Mnemonic#getMnemonicIndex()
-	 * 
-	 * @param name
-	 *            the mnemonic text resource name.
-	 * 
-	 * @throws ResourcesException
-	 *             if there was an error loading the text resource.
-	 */
-	public int getMnemonicIndex(String name) throws ResourcesException {
-		String keyname = mnemonics.getResource(name, locale).getText();
-		return mnemonicFactory.create(keyname).getMnemonicIndex();
-	}
+    /**
+     * Returns the displayed mnemonic index from the mnemonics texts resources
+     * if such index was set in the resource.
+     *
+     * @see Mnemonic#getMnemonicIndex()
+     *
+     * @param name
+     *            the mnemonic text resource {@link String} name.
+     *
+     * @return displayed mnemonic index or -1. The value -1 means no index was
+     *         specified.
+     *
+     * @throws ResourcesException
+     *             if there was an error loading the text resource.
+     */
+    public int getMnemonicIndex(String name) throws ResourcesException {
+        String keyname = mnemonics.getResource(name, locale).getText();
+        return mnemonicFactory.create(keyname).getMnemonicIndex();
+    }
 
-	/**
-	 * Loads a accelerator text resource with the specified name and returns the
-	 * accelerator key for an action.
-	 * 
-	 * @see Accelerator#getAccelerator()
-	 * 
-	 * @param name
-	 *            the accelerator text resource name.
-	 * 
-	 * @throws ResourcesException
-	 *             if there was an error loading the text resource.
-	 */
-	public KeyStroke getAccelerator(String name) {
-		try {
-			String keyname = accelerators.getResource(name, locale).getText();
-			return acceleratorFactory.create(keyname).getAccelerator();
-		} catch (MissingResourceException e) {
-			return null;
-		}
-	}
+    /**
+     * Returns the accelerator key for an action from the accelerators texts
+     * resources.
+     *
+     * @see Accelerator#getAccelerator()
+     *
+     * @param name
+     *            the accelerator text resource {@link String} name.
+     *
+     * @return accelerator {@link KeyStroke} or {@code null} if no accelerator
+     *         key was specified.
+     *
+     * @throws ResourcesException
+     *             if there was an error loading the text resource.
+     *
+     * @throws IllegalArgumentException
+     *             if the specified accelerator or the modifier are not a valid
+     *             key code.
+     */
+    public KeyStroke getAccelerator(String name) {
+        try {
+            String keyname = accelerators.getResource(name, locale).getText();
+            return acceleratorFactory.create(keyname).getAccelerator();
+        } catch (MissingResourceException e) {
+            return null;
+        }
+    }
 
-	public void addTextsLocaleListeners(LocaleListener l) {
-		localeListeners.addListener(l);
-	}
+    /**
+     * Adds the specified locale listener that is notified if a new locale is
+     * set for the texts resources.
+     *
+     * @param l
+     *            the {@link LocaleListener}.
+     */
+    public void addTextsLocaleListeners(LocaleListener l) {
+        localeListeners.addListener(l);
+    }
 
-	public void removeTextsLocaleListeners(LocaleListener l) {
-		localeListeners.removeListener(l);
-	}
+    /**
+     * Removes the specified locale listener that is notified if a new locale is
+     * set for the texts resources.
+     *
+     * @param l
+     *            the {@link LocaleListener}.
+     */
+    public void removeTextsLocaleListeners(LocaleListener l) {
+        localeListeners.removeListener(l);
+    }
 
 }
--- resources-texts/src/main/java/com/anrisoftware/resources/texts/texts/TextResourceImpl.java
+++ resources-texts/src/main/java/com/anrisoftware/resources/texts/texts/TextResourceImpl.java
@@ -45,175 +45,174 @@ import com.google.inject.assistedinject.Assisted;
 /**
  * Lazy load the text resource from the specified URL. The resource is
  * serializable to be used in a distributed cache.
- * 
+ *
  * @author Erwin Mueller, erwin.mueller@deventm.org
  * @since 1.0
  */
 class TextResourceImpl implements TextResource, Serializable {
 
-	/**
-	 * @since 1.0
-	 */
-	private static final long serialVersionUID = -2425350301703068585L;
+    /**
+     * @since 1.0
+     */
+    private static final long serialVersionUID = -2425350301703068585L;
 
-	/**
-	 * Wrapper around a character set to make the character set serializable.
-	 * 
-	 * @author Erwin Mueller, erwin.mueller@deventm.org
-	 * @since 1.0
-	 */
-	private static class SerializableCharsetWrapper implements Externalizable {
+    /**
+     * Wrapper around a character set to make the character set serializable.
+     *
+     * @author Erwin Mueller, erwin.mueller@deventm.org
+     * @since 1.0
+     */
+    private static class SerializableCharsetWrapper implements Externalizable {
 
-		private Charset charset;
+        private Charset charset;
 
-		/**
-		 * @deprecated Used only for serialization.
-		 */
-		@SuppressWarnings("unused")
-		@Deprecated
-		public SerializableCharsetWrapper() {
-		}
+        /**
+         * @deprecated Used only for serialization.
+         */
+        @Deprecated
+        public SerializableCharsetWrapper() {
+        }
 
-		/**
-		 * Sets the character set for serialization.
-		 * 
-		 * @param charset
-		 *            the {@link Charset}.
-		 */
-		public SerializableCharsetWrapper(Charset charset) {
-			this.charset = charset;
-		}
+        /**
+         * Sets the character set for serialization.
+         *
+         * @param charset
+         *            the {@link Charset}.
+         */
+        public SerializableCharsetWrapper(Charset charset) {
+            this.charset = charset;
+        }
 
-		/**
-		 * Returns the character set.
-		 * 
-		 * @return the {@link Charset}.
-		 */
-		public Charset getCharset() {
-			return charset;
-		}
+        /**
+         * Returns the character set.
+         *
+         * @return the {@link Charset}.
+         */
+        public Charset getCharset() {
+            return charset;
+        }
 
-		@Override
-		public void writeExternal(ObjectOutput out) throws IOException {
-			out.writeUTF(charset.name());
-		}
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+            out.writeUTF(charset.name());
+        }
 
-		@Override
-		public void readExternal(ObjectInput in) throws IOException,
-				ClassNotFoundException {
-			String charsetName = in.readUTF();
-			charset = Charset.forName(charsetName);
-		}
+        @Override
+        public void readExternal(ObjectInput in) throws IOException,
+                ClassNotFoundException {
+            String charsetName = in.readUTF();
+            charset = Charset.forName(charsetName);
+        }
 
-	}
+    }
 
-	private final TextResourceImplLogger log;
+    private final TextResourceImplLogger log;
 
-	private final BinaryResource binary;
+    private final BinaryResource binary;
 
-	private final SerializableCharsetWrapper charsetWrapper;
+    private final SerializableCharsetWrapper charsetWrapper;
 
-	private String text;
+    private String text;
 
-	private String formattedText;
+    private String formattedText;
 
-	/**
-	 * Sets the needed properties of the text resource.
-	 * 
-	 * @param logger
-	 *            the {@link TextResourceImplLogger} for logging messages.
-	 * 
-	 * @param factory
-	 *            the {@link BinaryResourceFactory} that creates the binary
-	 *            resource from which we load the text resource.
-	 * 
-	 * @param name
-	 *            the name of this resource.
-	 * 
-	 * @param locale
-	 *            the {@link Locale} of this resource.
-	 * 
-	 * @param charset
-	 *            the {@link Charset} of this resource.
-	 * 
-	 * @param url
-	 *            the {@link URL} of this resource.
-	 */
-	@Inject
-	TextResourceImpl(TextResourceImplLogger logger,
-			BinaryResourceFactory factory, @Assisted String name,
-			@Assisted Locale locale, @Assisted Charset charset,
-			@Assisted URL url) {
-		this.binary = factory.create(name, locale, url);
-		this.log = logger;
-		this.charsetWrapper = new SerializableCharsetWrapper(charset);
-	}
+    /**
+     * Sets the needed properties of the text resource.
+     *
+     * @param logger
+     *            the {@link TextResourceImplLogger} for logging messages.
+     *
+     * @param factory
+     *            the {@link BinaryResourceFactory} that creates the binary
+     *            resource from which we load the text resource.
+     *
+     * @param name
+     *            the name of this resource.
+     *
+     * @param locale
+     *            the {@link Locale} of this resource.
+     *
+     * @param charset
+     *            the {@link Charset} of this resource.
+     *
+     * @param url
+     *            the {@link URL} of this resource.
+     */
+    @Inject
+    TextResourceImpl(TextResourceImplLogger logger,
+            BinaryResourceFactory factory, @Assisted String name,
+            @Assisted Locale locale, @Assisted Charset charset,
+            @Assisted URL url) {
+        this.binary = factory.create(name, locale, url);
+        this.log = logger;
+        this.charsetWrapper = new SerializableCharsetWrapper(charset);
+    }
 
-	@Override
-	public String getName() {
-		return binary.getName();
-	}
+    @Override
+    public String getName() {
+        return binary.getName();
+    }
 
-	@Override
-	public Locale getLocale() {
-		return binary.getLocale();
-	}
+    @Override
+    public Locale getLocale() {
+        return binary.getLocale();
+    }
 
-	@Override
-	public URL getURL() {
-		return binary.getURL();
-	}
+    @Override
+    public URL getURL() {
+        return binary.getURL();
+    }
 
-	@Override
-	public Charset getCharset() {
-		return charsetWrapper.getCharset();
-	}
+    @Override
+    public Charset getCharset() {
+        return charsetWrapper.getCharset();
+    }
 
-	@Override
-	public String getText() throws ResourcesException {
-		if (text == null) {
-			readText();
-			binary.discardBinary();
-		}
-		return text;
-	}
+    @Override
+    public String getText() throws ResourcesException {
+        if (text == null) {
+            readText();
+            binary.discardBinary();
+        }
+        return text;
+    }
 
-	private void readText() throws ResourcesException {
-		InputStream stream = binary.getStream();
-		Charset charset = charsetWrapper.getCharset();
-		InputStreamReader reader = new InputStreamReader(stream, charset);
-		try {
-			text = IOUtils.toString(reader);
-		} catch (IOException e) {
-			throw log.errorLoadText(this, e);
-		}
-	}
+    private void readText() throws ResourcesException {
+        InputStream stream = binary.getStream();
+        Charset charset = charsetWrapper.getCharset();
+        InputStreamReader reader = new InputStreamReader(stream, charset);
+        try {
+            text = IOUtils.toString(reader);
+        } catch (IOException e) {
+            throw log.errorLoadText(this, e);
+        }
+    }
 
-	@Override
-	public String getFormattedText(Object... args) throws ResourcesException {
-		if (formattedText == null) {
-			formattedText = createformatText(args);
-		}
-		return formattedText;
-	}
+    @Override
+    public String getFormattedText(Object... args) throws ResourcesException {
+        if (formattedText == null) {
+            formattedText = createformatText(args);
+        }
+        return formattedText;
+    }
 
-	private String createformatText(Object[] args) throws ResourcesException {
-		String text = getText();
-		if (getLocale() != null) {
-			return format(getLocale(), text, args);
-		} else {
-			return format(text, args);
-		}
-	}
+    private String createformatText(Object[] args) throws ResourcesException {
+        String text = getText();
+        if (getLocale() != null) {
+            return format(getLocale(), text, args);
+        } else {
+            return format(text, args);
+        }
+    }
 
-	/**
-	 * String representation for debugging. Contains the locale, name and the
-	 * URL of this resource.
-	 */
-	@Override
-	public String toString() {
-		return new ToStringBuilder(this).append("locale", getLocale())
-				.append("name", getName()).append("url", getURL()).toString();
-	}
+    /**
+     * String representation for debugging. Contains the locale, name and the
+     * URL of this resource.
+     */
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("locale", getLocale())
+                .append("name", getName()).append("url", getURL()).toString();
+    }
 
 }
