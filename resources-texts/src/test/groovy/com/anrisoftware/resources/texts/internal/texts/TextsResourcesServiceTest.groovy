0cb960911d5cfc5cc846aea251bf3c44e314bfa3
Erwin Mueller <erwin.mueller@deventm.org>
2016-10-03 13:47:53 +0200

    Set year to 2016, the year the license was changed to the Apache 2.0.
    see #3633

--- resources-texts/src/test/groovy/com/anrisoftware/resources/texts/internal/texts/TextsResourcesServiceTest.groovy
+++ resources-texts/src/test/groovy/com/anrisoftware/resources/texts/internal/texts/TextsResourcesServiceTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2016 Erwin Müller <erwin.mueller@deventm.org>
+ * Copyright 2016 Erwin Müller <erwin.mueller@deventm.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 
 /*
  * Copyright 2012-2016 Erwin Müller <erwin.mueller@deventm.org>
