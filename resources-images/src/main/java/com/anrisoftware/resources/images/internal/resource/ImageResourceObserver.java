65bf40c87140f2eb4fd0168898b1efb9acbc151d
Erwin Müller <erwin.mueller@deventm.org>
2019-06-18 10:06:50 +0200

    Feature #4163 Fix sonarqube
    Rename field "width" to prevent any misunderstanding/clash with field "WIDTH" defined in interface "java.awt.image.ImageObserver".

--- resources-images/src/main/java/com/anrisoftware/resources/images/internal/resource/ImageResourceObserver.java
+++ resources-images/src/main/java/com/anrisoftware/resources/images/internal/resource/ImageResourceObserver.java
@@ -16,86 +16,83 @@
 
 package com.anrisoftware.resources.images.internal.resource;
 
-
 import java.awt.Image;
 import java.awt.image.ImageObserver;
 
 class ImageResourceObserver implements ImageObserver {
 
-	private final int flags;
+    private final int flags;
 
-	private int x;
+    private int x;
 
-	private int y;
+    private int y;
 
-	private int width;
+    private int imageWidth;
 
-	private int height;
+    private int imaheHeight;
 
-	private boolean done;
+    private boolean done;
 
-	/**
-	 * Sets the flags we observe for.
-	 * 
-	 * @param flags
-	 *            one or a combination of the flags of the image observer.
-	 * 
-	 * @see ImageObserver
-	 */
-	public ImageResourceObserver(int flags) {
-		this.flags = flags;
-		this.done = false;
-	}
+    /**
+     * Sets the flags we observe for.
+     * 
+     * @param flags one or a combination of the flags of the image observer.
+     * 
+     * @see ImageObserver
+     */
+    public ImageResourceObserver(int flags) {
+        this.flags = flags;
+        this.done = false;
+    }
 
-	@Override
-	public boolean imageUpdate(Image img, int infoflags, int x, int y,
-			int width, int height) {
-		if ((infoflags & flags) == flags) {
-			this.done = true;
-			this.x = x;
-			this.y = y;
-			this.width = width;
-			this.height = height;
-		}
-		return false;
-	}
+    @Override
+    public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
+        if ((infoflags & flags) == flags) {
+            this.done = true;
+            this.x = x;
+            this.y = y;
+            this.imageWidth = width;
+            this.imaheHeight = height;
+        }
+        return false;
+    }
 
-	/**
-	 * Returns <code>true</code> if the given flags is available from the image.
-	 */
-	public boolean isDone() {
-		return done;
-	}
+    /**
+     * Returns <code>true</code> if the given flags is available from the image.
+     */
+    public boolean isDone() {
+        return done;
+    }
 
-	/**
-	 * Returns the height of the image, if the {@link ImageObserver#HEIGHT} flag
-	 * becomes available.
-	 */
-	public int getHeight() {
-		return height;
-	}
+    /**
+     * Returns the height of the image, if the {@link ImageObserver#HEIGHT} flag
+     * becomes available.
+     */
+    public int getHeight() {
+        return imaheHeight;
+    }
 
-	/**
-	 * Returns the width of the image, if the {@link ImageObserver#WIDTH} flag
-	 * becomes available.
-	 */
-	public int getWidth() {
-		return width;
-	}
+    /**
+     * Returns the width of the image, if the {@link ImageObserver#WIDTH} flag
+     * becomes available.
+     */
+    public int getWidth() {
+        return imageWidth;
+    }
 
-	/**
-	 * Returns the X-axis of the pixels, if the {@link ImageObserver#SOMEBITS}
-	 * flag becomes available.
-	 */
-	public int getX() {
-		return x;
-	}
+    /**
+     * Returns the X-axis of the pixels, if the {@link ImageObserver#SOMEBITS} flag
+     * becomes available.
+     */
+    public int getX() {
+        return x;
+    }
 
-	/**
-	 * Returns the Y-axis of the pixels, if the {@link ImageObserver#SOMEBITS}
-	 * flag becomes available.
-	 */
-	public int getY() {
-		return y;
-	}
+    /**
+     * Returns the Y-axis of the pixels, if the {@link ImageObserver#SOMEBITS} flag
+     * becomes available.
+     */
+    public int getY() {
+        return y;
+    }
 }
