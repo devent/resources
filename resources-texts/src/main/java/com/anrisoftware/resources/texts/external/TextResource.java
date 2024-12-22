6a7c76b7a11a7e01f671fe4b06546c7cfa250452
Erwin Müller <erwin@muellerpublic.de>
2024-12-22 15:04:51 +0100

    Update license

--- resources-texts/src/main/java/com/anrisoftware/resources/texts/external/TextResource.java
+++ resources-texts/src/main/java/com/anrisoftware/resources/texts/external/TextResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2023 Erwin Müller <erwin.mueller@anrisoftware.com>
+ * Copyright 2012-2025 Erwin Müller <erwin.mueller@anrisoftware.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
